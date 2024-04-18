5c9cac462fde04885bfb7410e35b22ab848585eb
masaoikawa <masao.ikawa@me.com>
2024-04-19 03:16:46 +0900

    20240418 First Commit

--- src/StislaTwigBundle.php
+++ src/StislaTwigBundle.php
@@ -6,7 +6,7 @@ use ChapterThree\StislaTwigBundle\DependencyInjection\C3HelloExtension;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use \Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
-class C3StislaTwigBundle extends AbstractBundle
+class StislaTwigBundle extends AbstractBundle
 {
     public function getContainerExtension(): ?ExtensionInterface
     {
