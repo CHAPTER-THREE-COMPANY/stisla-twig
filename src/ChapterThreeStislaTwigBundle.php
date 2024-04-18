a42c16d20a6aaccd85ecb26d58d46532c45281a7
masaoikawa <masao.ikawa@me.com>
2024-04-19 02:01:04 +0900

    20240418 First Commit

--- src/ChapterThreeStislaTwigBundle.php
+++ src/ChapterThreeStislaTwigBundle.php
@@ -2,10 +2,20 @@
 
 namespace ChapterThree\StislaTwigBundle;
 
+use ChapterThree\StislaTwigBundle\DependencyInjection\C3HelloExtension;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use \Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
 class ChapterThreeStislaTwigBundle extends AbstractBundle
 {
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        if (!$this->extension) {
+            $this->extension = new C3HelloExtension();
+        }
+
+        return $this->extension;
+    }
     public function getPath(): string
     {
         return dirname(__DIR__);
