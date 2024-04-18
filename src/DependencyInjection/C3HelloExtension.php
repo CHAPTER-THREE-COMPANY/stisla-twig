b65c949599808baa8927df1d1258595a6eeadb00
masaoikawa <masao.ikawa@me.com>
2024-04-19 02:37:17 +0900

    20240418 First Commit

--- src/DependencyInjection/C3HelloExtension.php
+++ src/DependencyInjection/C3HelloExtension.php
@@ -6,13 +6,18 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class C3HelloExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
         // ... you'll load the files here later
-        $loader = new XmlFileLoader(
+        /*$loader = new XmlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../../Resources/config')
+        );*/
+        $loader = new YamlFileLoader(
             $container,
             new FileLocator(__DIR__.'/../../Resources/config')
         );
