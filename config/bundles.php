08e4c2d03c88b3e33522dcc440edf22dce42b461
masaoikawa <masao.ikawa@me.com>
2024-04-19 01:17:29 +0900

    20240418 First Commit

--- config/bundles.php
+++ config/bundles.php
@@ -3,5 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    \ChapterThree\StislaTwigBundle\ChapterThreeStislaTwigBundle::class => ['dev' => true],
+    ChapterThree\Stisla\Command\c3ConfigCommand::class => ['all' => true],
 ];
