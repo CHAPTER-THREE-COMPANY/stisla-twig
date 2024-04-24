3dcb2744373e2e39576b4ba5454495324ebec8a3
masaoikawa <masao.ikawa@me.com>
2024-04-24 15:54:19 +0900

    20240423 First Commit

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     ChapterThree\C3Bundle\C3Bundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
