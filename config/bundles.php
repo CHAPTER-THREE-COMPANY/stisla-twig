2845079ffdda94b6e4cc1d3920ff8d649cb57589
masaoikawa <masao.ikawa@me.com>
2024-04-25 22:41:22 +0900

    20240423 First Commit

--- config/bundles.php
+++ config/bundles.php
@@ -4,6 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    ChapterThree\C3Bundle\C3Bundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
--- src/Command/C3InstallCommand.php
+++ src/Command/C3InstallCommand.php
@@ -2,7 +2,6 @@
 
 namespace ChapterThree\C3Bundle\Command;
 
-use ChapterThree\C3Bundle\C3Stisla;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -12,10 +11,10 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
-    name: 'c3:config',
-    description: 'Add a short description for your command',
+    name: 'c3:install',
+    description: 'c3-bundle installation',
 )]
-class c3ConfigCommand extends Command
+class C3InstallCommand extends Command
 {
     public function __construct()
     {
@@ -33,23 +32,17 @@ class c3ConfigCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-
-        $io->ask("pause");
-        C3Stisla::info();
-        $io->ask("start");
-
-
         $arg1 = $input->getArgument('arg1');
 
         if ($arg1) {
             $io->note(sprintf('You passed an argument: %s', $arg1));
         }
 
-        /*if ($input->getOption('option1')) {
+        if ($input->getOption('option1')) {
+            // ...
+        }
 
-        }*/
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $io->success('インストールしますよ! Now make it your own! Pass --help to see your options.');
 
         return Command::SUCCESS;
     }
