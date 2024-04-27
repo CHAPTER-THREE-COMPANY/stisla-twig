98e43d00bd5b1c864cc58b1a7bc41e29f483dce2
masaoikawa <masao.ikawa@me.com>
2024-04-27 17:30:30 +0900

    20240427 First Commit

--- src/Twig/Components/S3Files.php
+++ src/Twig/Components/S3Files.php
@@ -4,7 +4,7 @@ namespace ChapterThree\C3Bundle\Twig\Components;
 
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
-#[AsTwigComponent]
+#[AsTwigComponent(template: '@C3/components/S3Files.html.twig')]
 final class S3Files
 {
 }
