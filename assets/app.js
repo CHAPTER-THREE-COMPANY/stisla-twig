25ca2d50f371cf8e699e153b982aed96e609d613
masaoikawa <masao.ikawa@me.com>
2024-05-19 06:18:56 +0900

    20240519 First Commit

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
 "use strict";
 console.log("OK")
 
-import 'vendor/@fortawesome/fontawesome-free/css/all.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css'
 import 'summernote/dist/summernote-bs4.css'
