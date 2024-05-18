f077ace48657499d381006236ec014dd01eea768
masaoikawa <masao.ikawa@me.com>
2024-05-19 02:41:54 +0900

    20240519 First Commit

--- importmap.php
+++ importmap.php
@@ -12,7 +12,7 @@
  * The "importmap:require" command can be used to add new entries to this file.
  */
 return [
-    'c3app' => [
+    '@C3/app' => [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
