2a0c88a833a30cfb3e6b5ddfb82dd5ecd636c573
masaoikawa <masao.ikawa@me.com>
2024-11-22 15:53:56 +0900

    20241122 First Commit

--- src/Twig/Components/C3/Fileuploader.php
+++ src/Twig/Components/C3/Fileuploader.php
@@ -4,7 +4,7 @@ namespace ChapterThree\C3Bundle\Twig\Components\C3;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 use Symfony\Component\Form\FormView;
 
-#[AsTwigComponent]
+#[AsTwigComponent(template: '@C3/components/Fileuploader.html.twig')]
 class Fileuploader
 {
     public FormView $form;
