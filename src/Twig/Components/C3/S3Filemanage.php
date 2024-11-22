1724fbd838707c422582bcda00f5b54fbe4729f8
masaoikawa <masao.ikawa@me.com>
2024-11-22 16:40:10 +0900

    20241122 First Commit

--- src/Twig/Components/C3/S3Filemanage.php
+++ src/Twig/Components/C3/S3Filemanage.php
@@ -4,8 +4,8 @@ namespace ChapterThree\C3Bundle\Twig\Components\C3;
 
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
-#[AsTwigComponent(template: '@C3/components/S3filemanage.html.twig')]
-class S3filemanage
+#[AsTwigComponent(template: '@C3/components/S3Filemanage.html.twig')]
+class S3Filemanage
 {
 //    public string $title;
 }
