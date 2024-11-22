e38ab989746278a66b793116b59896058ed97917
masaoikawa <masao.ikawa@me.com>
2024-11-22 16:19:27 +0900

    20241122 First Commit

--- src/Twig/Components/C3/Vue2.php
+++ src/Twig/Components/C3/Vue2.php
@@ -4,7 +4,7 @@ namespace ChapterThree\C3Bundle\Twig\Components\C3;
 
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
-#[AsTwigComponent]
+#[AsTwigComponent(template: '@C3/components/Vue2.html.twig')]
 class Vue2
 {
 //    public string $title;
