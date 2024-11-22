9d185738d2f423941d522ac4c1931581a94be94c
masaoikawa <masao.ikawa@me.com>
2024-11-22 16:04:21 +0900

    20241122 First Commit

--- src/Twig/Components/C3/Fileuploader.php
+++ src/Twig/Components/C3/Fileuploader.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace ChapterThree\C3Bundle\Twig\Components\C3;
+
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 use Symfony\Component\Form\FormView;
 
--- src/Twig/Components/C3/S3filemanage.php
+++ src/Twig/Components/C3/S3filemanage.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace ChapterThree\C3Bundle\Twig\Components\C3;
+
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
-#[AsTwigComponent]
+#[AsTwigComponent(template: '@C3/components/S3filemanage.html.twig')]
 class S3filemanage
 {
 //    public string $title;
